48f92aa575a59ecfc0edbf2e337544a33dee6031
Wadii Hajji <wadii@cardiologs.com>
2021-12-07 21:40:37 +0100

    Add day07

---
+++ 2021/07/07.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+(define (parse-ns in) (map string->number (string-split in ",")))
+(define input
+  (call-with-input-file "./input.txt"
+                        (lambda (in) (parse-ns (read-line in)))))
+(define max-pos (apply max input))
+(define (first-ns-sum n) (/ (* n (add1 n)) 2))
+
+(define (cheapest-position method)
+  (define (cost start current-min)
+    (define costs (apply + (map (method start) input)))
+    (if (eq? start max-pos)
+      current-min
+      (cost (add1 start) (if (< costs current-min) costs current-min))))
+  (cost 0 +inf.0))
+
+(define (constant-rate start) (lambda (x) (abs (- x start))))
+(define (non-constant-rate start) (lambda (x) (first-ns-sum (abs (- x start)))))
+
+(module+ test
+  (require rackunit)
+  (check-equal? (cheapest-position constant-rate) 331067)
+  (check-equal? (cheapest-position non-constant-rate) 92881128))
