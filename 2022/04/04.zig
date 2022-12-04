448b5eaa775c5d1668b8f01ba713d20df5a5e3a9
Wadii Hajji <wadii@cardiologs.com>
2022-12-04 13:35:03 +0100

    Rename methods

--- 2022/04/04.zig
+++ 2022/04/04.zig
@@ -5,19 +5,19 @@ const Section = struct {
     start: u8,
     end: u8,
 
-    pub fn contains(self: Section, id: u8) bool {
+    fn contains(self: Section, id: u8) bool {
         return self.start <= id and id <= self.end;
     }
 
-    pub fn covers(self: Section, other: Section) bool {
+    fn covers(self: Section, other: Section) bool {
         return self.start <= other.start and self.end >= other.end;
     }
 
-    pub fn overlaps(self: Section, other: Section) bool {
+    fn overlaps(self: Section, other: Section) bool {
         return self.contains(other.start) or self.contains(other.end);
     }
 
-    pub fn fromIds(ids: []const u8) !Section {
+    fn fromIds(ids: []const u8) !Section {
         var parts = std.mem.split(u8, ids, "-");
         return Section{
             .start = try std.fmt.parseUnsigned(u8, parts.next().?, 10),
@@ -30,15 +30,15 @@ const Pair = struct {
     first_elf: Section,
     second_elf: Section,
 
-    pub fn fullyContains(self: Pair) bool {
+    fn fullyContains(self: Pair) bool {
         return self.first_elf.covers(self.second_elf) or self.second_elf.covers(self.first_elf);
     }
 
-    pub fn partiallyContains(self: Pair) bool {
+    fn partiallyContains(self: Pair) bool {
         return self.first_elf.overlaps(self.second_elf) or self.second_elf.overlaps(self.first_elf);
     }
 
-    pub fn fromAssignment(assignment: []const u8) !Pair {
+    fn fromAssignment(assignment: []const u8) !Pair {
         var parts = std.mem.split(u8, assignment, ",");
         return Pair{
             .first_elf = try Section.fromIds(parts.next().?),
@@ -47,7 +47,7 @@ const Pair = struct {
     }
 };
 
-pub fn countUselessWork(assignments: []const u8) !usize {
+fn part1(assignments: []const u8) !usize {
     var parts = std.mem.split(u8, assignments, "\n");
     var count: usize = 0;
     while (parts.next()) |s| {
@@ -62,7 +62,7 @@ pub fn countUselessWork(assignments: []const u8) !usize {
     return count;
 }
 
-pub fn countDuplicatedWork(assignments: []const u8) !usize {
+fn part2(assignments: []const u8) !usize {
     var parts = std.mem.split(u8, assignments, "\n");
     var count: usize = 0;
     while (parts.next()) |s| {
@@ -79,14 +79,14 @@ pub fn countDuplicatedWork(assignments: []const u8) !usize {
 
 test "part1" {
     const sample = @embedFile("./sample.txt");
-    try std.testing.expectEqual(countUselessWork(sample), 2);
+    try std.testing.expectEqual(part1(sample), 2);
     const input = @embedFile("./input.txt");
-    try std.testing.expectEqual(countUselessWork(input), 475);
+    try std.testing.expectEqual(part1(input), 475);
 }
 
 test "part2" {
     const sample = @embedFile("./sample.txt");
-    try std.testing.expectEqual(countDuplicatedWork(sample), 4);
+    try std.testing.expectEqual(part2(sample), 4);
     const input = @embedFile("./input.txt");
-    try std.testing.expectEqual(countDuplicatedWork(input), 825);
+    try std.testing.expectEqual(part2(input), 825);
 }
