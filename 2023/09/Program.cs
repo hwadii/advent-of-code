b9d24718c7bf972ac60ab72e9972fee99f2668a9
Wadii Hajji <wadii@cardiologs.com>
2023-12-09 12:51:34 +0100

    Simplify

--- 2023/09/Program.cs
+++ 2023/09/Program.cs
@@ -17,55 +17,32 @@ Part2();
 
 class History
 {
-    private Sequence _nums;
+    private List<int> _nums;
 
     public History(string nums)
     {
-        _nums = new Sequence(nums.Split(' ').Select(n => int.Parse(n)).ToList());
+        _nums = nums.Split(' ').Select(n => int.Parse(n)).ToList();
     }
 
     public int Next()
     {
-        var next = _nums.Last;
+        var next = _nums.Last();
 
-        while (!_nums.Zeroes)
+        while (!_nums.All(n => n == 0))
         {
-            _nums = _nums.Next();
-            next += _nums.Last;
+            var nums = new List<int>();
+
+            for (var i = 0; i < _nums.Count - 1; i++)
+            {
+                nums.Add(_nums[i + 1] - _nums[i]);
+            }
+
+            next += nums.Last();
+            _nums = nums;
         }
 
         return next;
     }
-
-    public Sequence Nums => _nums;
-}
-
-class Sequence
-{
-    private readonly List<int> _nums;
-
-    public Sequence(List<int> nums)
-    {
-        _nums = nums;
-    }
-
-    public Sequence Next()
-    {
-        var nums = new List<int>();
-
-        for (var i = 0; i < _nums.Count - 1; i++)
-        {
-            nums.Add(_nums[i + 1] - _nums[i]);
-        }
-
-        return new Sequence(nums);
-    }
-
-    public int First => _nums.First();
-
-    public int Last => _nums.Last();
-
-    public bool Zeroes => _nums.All(n => n == 0);
 
     public List<int> Nums => _nums;
 }
