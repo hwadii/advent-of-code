ff2c6c7eb099003ce824382ea6880229a9fde193
Wadii Hajji <hajji.wadii@yahoo.fr>
2024-12-07 16:36:50 +0100

    6

--- 2024/06/06.scala
+++ 2024/06/06.scala
@@ -8,7 +8,7 @@ import java.lang.IndexOutOfBoundsException
   println(part1("input.txt"))
 
 @main def part2: Unit =
-  println(part2("sample.txt"))
+  println(part2("input.txt"))
 
 def readFile(path: String): String =
   Source.fromFile(path).mkString
@@ -29,26 +29,25 @@ class Point(val x: Int, val y: Int):
     s"($x, $y)"
 
 class Guard(var position: Point, var direction: Direction):
-  def move(direction: Direction) =
-    this.position += direction.point
-    this.direction = direction
-
-  def next: Direction =
+  def nextDirection: Direction =
     this.direction match
       case Direction.E => Direction.S
       case Direction.S => Direction.W
       case Direction.N => Direction.E
       case Direction.W => Direction.N
 
-  def prev: Direction =
-    this.direction match
-      case Direction.E => Direction.N
-      case Direction.S => Direction.E
-      case Direction.N => Direction.W
-      case Direction.W => Direction.S
-
-  def cycled(other: Guard): Boolean =
-    position == other.position && direction == other.prev
+  def move(predicate: Point => Boolean): Guard =
+    var direction = this.direction
+    var tentative = this.position + this.direction.point
+    while (predicate(tentative)) {
+      direction = direction match
+        case Direction.E => Direction.S
+        case Direction.S => Direction.W
+        case Direction.N => Direction.E
+        case Direction.W => Direction.N
+      tentative = this.position + direction.point
+    }
+    Guard(tentative, direction)
 
   override def toString: String =
     s"$position: $direction"
@@ -64,66 +63,45 @@ enum Direction(val point: Point):
   case E extends Direction(Point(1, 0))
   case N extends Direction(Point(0, -1))
 
+def positions(map: List[List[String]], guard: Guard): List[Point] =
+  var positions = List[Point](guard.position)
+  var moving = guard
+  try {
+    while (true) {
+      moving = moving.move(t => map(t.y)(t.x) == "#")
+      positions = positions.appended(moving.position)
+    }
+    positions.distinctBy((p) => (p.x, p.y))
+  } catch {
+    case ex: IndexOutOfBoundsException => positions.distinctBy((p) => (p.x, p.y))
+  }
+
 def part1(path: String): Int =
   val contents = readFile(path)
   val start = contents.replaceAll("\n", "").zipWithIndex.find((c, _) => c == '^').get
   val map = contents.split("\n").map(_.split("").toList).toList
-  val bounds = Point(map(0).length, map.length)
-  var position = Point(start._2 % bounds.x, start._2 / bounds.y)
-  var direction = Direction.N
-  var positions = List[Point](position)
-  def safe(position: Point, direction: Direction): Boolean =
-    (position + direction.point).within(bounds)
-  val next = (direction: Direction) =>
-    direction match
-      case Direction.E => Direction.S
-      case Direction.S => Direction.W
-      case Direction.N => Direction.E
-      case Direction.W => Direction.N
-  try {
-    while (true) {
-      val tentative = position + direction.point
-      if (map(tentative.y)(tentative.x) == "#") {
-        direction = next(direction)
-        position = position + direction.point
-      } else {
-        position = position + direction.point
-      }
-      positions = positions.appended(position)
-    }
-    positions.distinctBy((p) => (p.x, p.y)).length
-  } catch {
-    case ex: IndexOutOfBoundsException => positions.distinctBy((p) => (p.x, p.y)).length
-  }
+  val position = Point(start._2 % map(0).length, start._2 / map.length)
+  val guard = Guard(position, Direction.N)
+  positions(map, guard).length
 
 def part2(path: String): Int =
   val contents = readFile(path)
   val start = contents.replaceAll("\n", "").zipWithIndex.find((c, _) => c == '^').get
-  def parseMap(idx: Int): List[List[String]] =
-    contents.patch(idx, "#", 1).split("\n").map(_.split("").toList).toList
-  def isCycle(idx: Int): Boolean =
-    val map = parseMap(idx)
-    println(map.map(_.mkString).mkString("\n"))
-    println(map)
-    val bounds = Point(map(0).length, map.length)
-    val initial = Guard(Point(start._2 % bounds.x, start._2 / bounds.y), Direction.N)
+  val map = contents.split("\n").map(_.split("").toList).toList
+  val position = Point(start._2 % map(0).length, start._2 / map.length)
+  val initial = Guard(position, Direction.N)
+  def isCycle(obstacle: Point): Boolean =
     var moving: Guard | Null = null
+    var iter = 0
     try {
-      while (moving.cycled(initial)) {
+      while (iter < 25_000) {
         if (moving == null)
           moving = Guard(initial.position, initial.direction)
-        val tentative = moving.position + moving.direction.point
-        if (map(tentative.y)(tentative.x) == "#") {
-          moving.direction = moving.next
-          moving.position = moving.position + moving.direction.point
-        } else {
-          moving.position = moving.position + moving.direction.point
-        }
-        println(s"$initial\t$moving")
+        moving = moving.move(t => t == obstacle || map(t.y)(t.x) == "#")
+        iter += 1
       }
-      true
+      iter == 25_000
     } catch {
       case ex: IndexOutOfBoundsException => false
     }
-  println(isCycle(69))
-  0
+  positions(map, initial).filter(_ != position).filter(isCycle(_)).length
